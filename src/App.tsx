53e706adc03e263cef76637296e12018b94d420f
dalram <ramonas.dalius@gmail.com>
2023-03-02 14:27:11 +0200

    App.tsx input for city

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import type { RootState } from "./app/store";
 import "./App.css";
 import { Counter } from "./components/counter";
@@ -7,6 +7,7 @@ import { weatherByCity } from "./app/weatherDataSlice";
 import { useWeatherData } from "./hooks/useWeatherData";
 
 function App() {
+  const [cityInput, setCityInput] = useState("");
   const weatherInfo = useSelector(
     (state: RootState) => state.weather.weatherData
   );
@@ -17,13 +18,33 @@ function App() {
   useEffect(() => {
     fetchWeatherData("Vilnius");
   }, []);
-
+  console.log("render");
+  const handleInputValue = (cityInput: string): void => {
+    fetchWeatherData(cityInput);
+  };
   return (
     <>
-      <div>{weatherInfo.name}</div>
-      <div>{weatherInfo.main.feels_like.toFixed()}</div>
-      <div>{weatherInfo.weather[0].description}</div>
-      <img src={iconUrl} alt={weatherInfo.weather[0].description} />
+      <div>
+        <div className="container">
+          <input
+            type="text"
+            className="city-input"
+            onChange={(e) => setCityInput(e.target.value)}
+          />
+          <button onClick={() => handleInputValue(cityInput)}>
+            Show city weather
+          </button>
+
+          <p>{weatherInfo.name}</p>
+          <p>{weatherInfo.main.feels_like.toFixed()}</p>
+          <p>{weatherInfo.weather[0].description}</p>
+          <img
+            src={iconUrl}
+            alt={weatherInfo.weather[0].description}
+            className="weather-icon"
+          />
+        </div>
+      </div>
       {/* <Counter /> */}
     </>
   );
